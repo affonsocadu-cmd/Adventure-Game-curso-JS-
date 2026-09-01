15a0a53ccc66f32a80834352240340e377c80fe0
affonsocadu-cmd <affonso.cadu@gmail.com>
2026-09-01 11:24:34 -0300

    Organiza projeto na raiz do repositorio

---
+++ curso/learning.js
@@ -0,0 +1,58 @@
+console.log()
+let userName = "Carlos"
+let userAge = 18;
+let isStudent = true;
+// Display the profile informations
+console.log("User Profile:");
+console.log("Name: " + userName);
+console.log("Age: " + userAge);
+console.log("Student: " + isStudent);
+console.log() 
+// Crating product information
+let productName = "Coffe";
+let productPrice = 15.99;
+let inStock = false;
+let productDescription = "Large coffe without sugar or milk, only black coffe."
+// Display the product infomations
+console.log("Product Details:")
+console.log("Product Name: " + productName);
+console.log("Price: " + productPrice +"$");
+console.log("Avaliable: " + inStock)
+console.log("Product Description: " + productDescription)
+console.log()
+
+// Math and numbers
+let wholeNumber = 42;
+let decimalNumber = 3.321;
+let negativeNumBer = -23;
+
+//basic operations
+let sum = wholeNumber + decimalNumber
+let difference = wholeNumber - negativeNumBer
+
+ //display results
+ console.log("Numbers examples:");
+ console.log("Whole number: " + wholeNumber);
+ console.log("Decimal number: " + decimalNumber);
+ console.log("Negative number: " + negativeNumBer)
+ console.log("Sum: " + sum)
+ console.log("Difference: " + difference)
+ console.log()
+
+ //Combining everything
+let intemName = "Laptop";
+let itemPrice = 1499.99;
+let itemQuantity = 1;
+let isAvaliable = true;
+let itemDescription = "Black laptop gamer with 512GB of storage and 32GB of RAM";
+
+let totalCost = itemPrice * itemQuantity;
+
+console.log("Carlãos Informatica");
+console.log("Item: " + intemName);
+console.log("Quantity: " + itemQuantity);
+console.log("Price: " + itemPrice + "$");
+console.log("Description: " + itemDescription);
+console.log("Avaliable: " + isAvaliable)
+console.log("-----------------------------------------------------------")
+console.log("Total: " + totalCost + "$")
---
+++ datatype.js
@@ -0,0 +1,26 @@
+// JavaScript Data Types
+
+//String:
+let username = "Carlos";
+
+// Numbers
+let userage = 18
+
+// booleans
+
+let SubscribedToNewLetters = true;
+
+
+//Social Media Post
+
+let post = {
+    author : "Carlos",
+    content : "Just bought a new Pc Gamer on Pichau !",
+    timestamp: "2026-08-15T14:30:00",
+    likes : 42,
+    comments : ["Congratulations !", "Damn", "lol"]
+};
+
+let currentUser = true;
+
+console.log(post)
