d1acf7a31917c4c1a62053534eff30fb18d7f528
affonsocadu-cmd <affonso.cadu@gmail.com>
2026-09-01 12:24:10 -0300

    Exercícios de Cálculo básicos

---
+++ curso/basicCalculations.js
@@ -0,0 +1,21 @@
+// Basic Calculations
+
+//addition
+let sum =  5 + 3;
+console.log("Sum: " + sum);
+
+// substraction
+let difference = 10 - 4;
+console.log("Difference: " + difference);
+
+// multiplication
+let product = 6 * 7;
+console.log("Product: " + product);
+
+// division
+let quotient = 20 / 5;
+console.log("Quotient: " + quotient);
+
+// remainder
+let remainder = 20 % 5;
+console.log("Remainder: " + remainder);
