41897181684a44316606f1d13b21ca38f18ac8c9
Carlos <seu_email@exemplo.com>
2026-09-10 18:58:32 -0300

    Arrays

---
+++ Adventure-Game-curso-JS--master/curso/basicArrays.js
@@ -0,0 +1,25 @@
+// Basic Arrays
+
+// Basic array creation
+console.log("Array Basics: ");
+
+// empty array
+let emptyArray = [];
+console.log("Empty array: ", emptyArray);
+
+// array with items
+let fruitsArray = ['apple', 'banana', 'oragne'];
+console.log("Fruits array: ", fruitsArray)
+
+// array with mixed data types
+let mixedArray = [42, 'hello', true, null];
+console.log("Mixed data types array: ", mixedArray);
+
+// array length
+console.log("Fruits array length: ", fruitsArray.length) ;
+
+// using toString() method
+console.log("Fruits as string: ", fruitsArray.toString());
+
+// arrays are zero-indexed
+console.log("First fruit: ",fruitsArray[0]);
